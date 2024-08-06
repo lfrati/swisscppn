237cca7d785c043139635ee9dfe8f6d26f3abadd
lfrati <fratilapo@gmail.com>
2024-08-06 23:26:13 +0200

    Use texture to store network params.
    How can I use sampler2DArray to store all the tiles state and not have to recompute them over and over?

--- utils.js
+++ utils.js
@@ -1,31 +1,24 @@
-function minit() {
-  return (Math.random() * 2 - 1) * 1.5;
-}
-
-function binit() {
-  return Math.random();
-}
-
-function mat4init() {
-  return `mat4(${minit()},${minit()},${minit()},${minit()},${minit()},${minit()},${minit()},${minit()},${minit()},${minit()},${minit()},${minit()},${minit()},${minit()},${minit()},${minit()})`;
-}
-
-function vec4init() {
-  return `vec4(${binit()},${binit()},${binit()},${binit()})`;
-}
-
+// TODO: remove hardcoded tex/row names?
 function make_init(params) {
-  let init = ["Net("];
+  let body = [];
+  let cnt = 0;
   for (let i in params) {
     p = params[i];
-    if (i > 0) {
-      init.push(", ");
+    switch (p[0]) {
+      case "w":
+        body.push(`get_mat4(row, ${cnt})`);
+        cnt += 4;
+        break;
+      case "b":
+        body.push(`get_vec4(row, ${cnt})`);
+        cnt += 1;
+        break;
+      default:
+        console.error("UNKNOWN value:", p);
     }
-    if (p[0] == "w") init.push(`${mat4init()}`);
-    else init.push(`${vec4init()}`);
   }
-  init.push(")");
-  return init.join("");
+  let init = `Net(${body.join(",\n    ")}\n)`;
+  return [init, cnt + 1];
 }
 
 function make_net(params) {
@@ -38,55 +31,51 @@ function make_net(params) {
   return net;
 }
 
-function make_lerp(params) {
-  lerp_fun = "Net lerp(in Net n1, in Net n2, in float t){\n  Net n3;\n";
-  for (let p of params) {
-    lerp_fun += `  n3.${p} = lerp(n1.${p}, n2.${p}, t);\n`;
-  }
-  lerp_fun += "  return n3;\n}\n";
-  return lerp_fun;
-}
-
 /**
-vec4 f(vec4 inputs, Net n){
-  vec4 h1_1 = relu_norm(inputs * n.w1_1 + n.b1_1);
-  vec4 h1_2 = relu_norm(inputs * n.w1_2 + n.b1_2);
-  vec4 h1 = h1_1 + h1_2 ;
-  vec4 h2_1 = cos((h1 * n.w2_2)*0.5 + n.b2_2);
-  vec4 h2_2 = cos((h1 * n.w2_1)*0.5 + n.b2_1);
-  vec4 h2 = h2_1 + h2_2 ;
-  return fsig(h2 * n.w3);
-}
-
-mat4 lerp(mat4 w1, mat4 w2, float t){return w1 + (w2 - w1)*t;}
-vec4 lerp(vec4 w1, vec4 w2, float t){return w1 + (w2 - w1)*t;}
-void lerp(inout Net n1, in Net n2, const float t){
-          n1.w1_1 = lerp(n1.w1_1, n2.w1_1, t);
-          n1.w1_2 = lerp(n1.w1_2, n2.w1_2, t);
-          n1.w2_1 = lerp(n1.w2_1, n2.w2_1, t);
-          n1.w2_2 = lerp(n1.w2_2, n2.w2_2, t);
-          n1.w3 =  lerp(n1.w3, n2.w3, t);
-          n1.b1_1 = lerp(n1.b1_1, n2.b1_1, t);
-          n1.b1_2 = lerp(n1.b1_2, n2.b1_2, t);
-          n1.b2_1 = lerp(n1.b2_1, n2.b2_1, t);
-          n1.b2_2 = lerp(n1.b2_2, n2.b2_2, t);
-}
-*/
-
-function gen_lerp(val) {
-  return `  n1.${val} = lerp(n1.${val}, n2.${val}, t);`;
-}
+ *
+ * struct Net{
+ *   mat4 w1_0;
+ *   vec4 b1_0;
+ *   mat4 w1_1;
+ *   vec4 b1_1;
+ *   mat4 w3_0;
+ *   vec4 b3_0;
+ *   mat4 w3_1;
+ *   vec4 b3_1;
+ *   mat4 w4_0;
+ *   vec4 b4_0;
+ * };
+ *
+ * Net(get_mat4(tex, row, 0),
+ *   get_vec4(tex, row, 4),
+ *   get_mat4(tex, row, 5),
+ *   get_vec4(tex, row, 9),
+ *   get_mat4(tex, row, 10),
+ *   get_vec4(tex, row, 14),
+ *   get_mat4(tex, row, 15),
+ *   get_vec4(tex, row, 19),
+ *   get_mat4(tex, row, 20),
+ *   get_vec4(tex, row, 24)
+ * );
+ *
+ * vec4 f(vec4 inputs, Net n){
+ *   vec4 h1_1 = relu_norm(inputs * n.w1_1 + n.b1_1);
+ *   vec4 h1_2 = relu_norm(inputs * n.w1_2 + n.b1_2);
+ *   vec4 h1 = h1_1 + h1_2 ;
+ *   vec4 h2_1 = cos((h1 * n.w2_2)*0.5 + n.b2_2);
+ *   vec4 h2_2 = cos((h1 * n.w2_1)*0.5 + n.b2_1);
+ *   vec4 h2 = h2_1 + h2_2 ;
+ *   return fsig(h2 * n.w3);
+ * }
+ *
+ *
+ */
 
 function parse(conf) {
   /*
-   * All weights are 4x4 all biases are 4x1
+   * All weights are 4x4, all biases are 4x1
    */
   let forward = ["vec4 forward(in vec4 x0, in Net net){"];
-  let lerp = [
-    `mat4 lerp(mat4 w1, mat4 w2, float t){return w1 + (w2 - w1)*t;}
-vec4 lerp(vec4 w1, vec4 w2, float t){return w1 + (w2 - w1)*t;}
-void lerp(inout Net n1, in Net n2, const float t){`,
-  ];
   let params = [];
   let depth = 0;
   let inp, out;
@@ -107,12 +96,9 @@ void lerp(inout Net n1, in Net n2, const float t){`,
           partials.push(partial);
           let weight = `w${depth}_${branch}`;
           params.push(weight);
-          lerp.push(gen_lerp(weight));
-          lerp.push();
           if (affine) {
             let bias = `b${depth}_${branch}`;
             params.push(bias);
-            lerp.push(gen_lerp(bias));
             forward.push(
               `  vec4 ${partial} = ${layer.f}(x${inp} * net.${weight} + net.${bias});`
             );
@@ -130,11 +116,9 @@ void lerp(inout Net n1, in Net n2, const float t){`,
         // to avoid creating intermediate values
         let weight = `w${depth}_0`;
         params.push(weight);
-        lerp.push(gen_lerp(weight));
         if (affine) {
           let bias = `b${depth}_0`;
           params.push(bias);
-          lerp.push(gen_lerp(bias));
           forward.push(
             `  vec4 x${out} = ${layer.f}(x${inp} * net.${weight} + net.${bias});`
           );
@@ -150,7 +134,6 @@ void lerp(inout Net n1, in Net n2, const float t){`,
     }
   }
   forward.push(`  return x${out};\n}\n`);
-  lerp.push("}");
 
   let decl = [];
   for (let val of params) {
@@ -158,6 +141,6 @@ void lerp(inout Net n1, in Net n2, const float t){`,
   }
   decl = ["struct Net{\n" + decl.join("\n") + "\n};\n"];
 
-  let code = decl.concat(forward).concat(lerp);
+  let code = decl.concat(forward);
   return [code.join("\n"), params];
 }
